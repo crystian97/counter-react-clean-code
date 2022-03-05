529c06e22145cff75f70fa911b5ce0320f69fc90
crystian97 <icrystianveloso@gmail.com>
2022-03-05 13:15:41 -0300

    Counter Using Clean Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+const COUNTDOWN_INITIAL_TIME_SECONDS = 1*60;//25 MINUTES
 
 function App() {
+  const [secondsAmount,setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_SECONDS)
+  const minutes= Math.floor(secondsAmount/60);
+  const seconds =(secondsAmount % 60);
+
+  useEffect(()=>{
+   if(secondsAmount>0){
+    setTimeout(()=>{
+      setSecondsAmount(state =>state -1);
+    },1000)
+   }else{
+     alert('Chegou ao fim!');
+   }
+   
+  },[secondsAmount])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <span> {String(minutes).padStart(2,'0')}</span>
+     <span>:</span>
+     <span>{String(seconds).padStart(2,'0')}</span>
+
+   </div>
+
   );
 }
 
